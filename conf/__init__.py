c335e563873246ed0cc7e39a09cc53b0cfcf44b0
王俊泽 <wangjunze@enn.cn>
2019-09-26 14:52:18 +0800

    Change config structure

--- conf/__init__.py
+++ conf/__init__.py
@@ -1,1 +1,9 @@
-from .config import *
+import os
+config_name = os.environ.get('CONFIG_NAME') or 'development'
+
+if config_name == 'production':
+    from .config_prod import *
+elif config_name == 'test':
+    from .config_test import *
+else:  # development
+    from .config_dev import *
