6172e42a6fe1cba3da87ff56a0f80630dbadf350
王俊泽 <wangjunze@enn.cn>
2019-10-14 17:47:37 +0800

    Set number of workers

--- conf/gunicorn_conf.py
+++ conf/gunicorn_conf.py
@@ -5,7 +5,7 @@ import multiprocessing
 bind = '0.0.0.0:80'
 
 # reload = True
-workers = min(multiprocessing.cpu_count() * 2 + 1, 17)
+workers = min(multiprocessing.cpu_count() * 2 + 1, 5)
 worker_class = 'gevent'
 
 x_forwarded_for_header = 'X-FORWARDED-FOR'
