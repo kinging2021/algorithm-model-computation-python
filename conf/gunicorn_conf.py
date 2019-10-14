588a12b186ac8c2d09a1b3adf2c56c941ea1dadd
王俊泽 <wangjunze@enn.cn>
2019-10-14 18:34:35 +0800

    reset

--- conf/gunicorn_conf.py
+++ conf/gunicorn_conf.py
@@ -5,7 +5,7 @@ import multiprocessing
 bind = '0.0.0.0:80'
 
 # reload = True
-workers = min(multiprocessing.cpu_count() * 2 + 1, 5)
+workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'gevent'
 
 x_forwarded_for_header = 'X-FORWARDED-FOR'
