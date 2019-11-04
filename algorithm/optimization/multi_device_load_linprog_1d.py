8e3f726810d802ded75a74d743ee64db82a2f48e
王俊泽 <wangjunze@enn.cn>
2019-11-04 11:46:08 +0800

    MultiDeviceLoadLinprog1D works

--- algorithm/optimization/multi_device_load_linprog_1d.py
+++ algorithm/optimization/multi_device_load_linprog_1d.py
@@ -3,6 +3,7 @@
 import itertools
 import numpy as np
 from scipy.optimize import linprog
+from scipy.interpolate import interp1d
 from algorithm.exception import ParamError
 
 
@@ -14,7 +15,8 @@ class MultiDeviceLoadLinprog1D(object):
         self.schedule_num = schedule_num
         self.schedule_list = [None] * self.schedule_num
         self._bins = []
-        self._rated_outputs = np.array([device['rated_output'] for device in self.devices])
+        self._rated_sum = 0
+        self._bin_list = []
 
         self.__arg_check()
 
@@ -24,45 +26,57 @@ class MultiDeviceLoadLinprog1D(object):
         return self.schedule_list
 
     def get_bins(self):
-        rated_sum = 0
+        bins = []
         for device in self.devices:
-            rated_sum += device['rated_output'] * device['load_max'] / 100
-        if rated_sum < self.target_output:
-            msg = 'The maximum rated output of all device is less than \'target_output\'. \n'
-            for i, device in enumerate(self.devices):
-                if device.get('load_max_input'):
-                    msg += 'device[%d][\'load_max\'] is %d, ' \
-                           'but the max load in device[%d][\'cost_data\'] is %d \n' \
-                           % (i, device['load_max_input'], i, device['load_max'])
-            raise ParamError(msg)
+            load_min_calc = self.target_output - (self._rated_sum - device['output_max'])
 
-        bins = [0]
-        for device in self.devices:
-            load_min_calc = self.target_output - (rated_sum - device['rated_output'] * device['load_max'] / 100)
-            data = np.array(device['cost_data'])
             if load_min_calc < 0:
                 device['required'] = False
-                index = (data[:, 0] >= device['load_min']) & (data[:, 0] <= device['load_max'])
+                bound_min = device['load_min_real']
             else:
                 device['required'] = True
-                index = (data[:, 0] >= max(device['load_min'], load_min_calc / device['rated_output'])) \
-                        & (data[:, 0] <= device['load_max'])
-            data = data[index]
-            data[:, 0] /= 100
-            data = data[data[:, 0].argsort()]
-            device['x'] = data[:, 0]
-            device['y'] = data[:, 1]
-            bins += device['x'].tolist()
+                bound_min = max(device['load_min_real'], load_min_calc / device['rated_output'])
+
+            bound_max = device['load_max_real']
+
+            x = device['data'][:, 0]
+            y = device['data'][:, 1]
+
+            func = interp1d(x, y)
+            if bound_min not in x:
+                p_min = [bound_min, func(bound_min)]
+                device['data'] = np.vstack((device['data'], p_min))
+            if bound_max not in x:
+                p_max = [bound_max, func(bound_max)]
+                device['data'] = np.vstack((device['data'], p_max))
+
+            index = (device['data'][:, 0] >= bound_min) & (device['data'][:, 0] <= bound_max)
+
+            device["data"] = device["data"][index]
+            device["data"] = device["data"][device["data"][:, 0].argsort()]
+
+            device['x'] = device["data"][:, 0]
+            device['y'] = device["data"][:, 1]
+            b = device['x'].tolist()
+            device['bin_start'] = b[0]
+            device['bin_stop'] = b[-1]
+            bins += b
         self._bins = list(set(bins))
         self._bins.sort()
 
+        for device in self.devices:
+            start = self._bins.index(device['bin_start']) + 1
+            stop = self._bins.index(device['bin_stop']) + 1
+            bins = self._bins[start:stop]
+            if not device['required']:
+                bins.insert(0, 0)
+            self._bin_list.append(bins)
+
     def solve(self):
         cost_list = [float('inf')] * self.schedule_num
-
-        for bins_right in itertools.product(self._bins, repeat=len(self.devices)):
-            if not self.__check_feasible_region(bins_right):
-                continue
-            output_sum = (np.array(bins_right) * self._rated_outputs).sum()
+        rated_outputs = np.array([device['rated_output'] for device in self.devices])
+        for bins_right in itertools.product(*self._bin_list):
+            output_sum = np.matmul(bins_right, rated_outputs)
             if output_sum < self.target_output:
                 continue
 
@@ -94,7 +108,7 @@ class MultiDeviceLoadLinprog1D(object):
         # 产量约束 A_ub := [[rated_output_1, rated_output_2, rated_output_3]]
         #         b_ub := [target_output]
         A_ub = [[]]
-        b_ub = [self.target_output]
+        b_ub = [-self.target_output]
         for i, bin_right in enumerate(bins_right):
             if bin_right == 0:
                 unused_device.append(i)
@@ -108,9 +122,9 @@ class MultiDeviceLoadLinprog1D(object):
             b = y1 - k * x1
             c.append(k)
             bias += b
-            bin_left = self._bins[self._bins.index(bins_right) - 1]
+            bin_left = self._bins[self._bins.index(bin_right) - 1]
             bounds.append((bin_left, bin_right))
-            A_ub[0].append(self.devices[i]['rated_output'])
+            A_ub[0].append(-self.devices[i]['rated_output'])
         kwargs = {
             'c': c,
             'A_ub': A_ub,
@@ -119,23 +133,30 @@ class MultiDeviceLoadLinprog1D(object):
         }
         return kwargs, bias, unused_device
 
-    def __check_feasible_region(self, bins_right):
-        for i, bin_ in enumerate(bins_right):
-            if bin_ == 0:
-                if self.devices[i]['required']:
-                    return False
-                else:
-                    return True
-            if bin_ <= self.devices[i]['load_min']:
-                return False
-
     def __arg_check(self):
+        rated_sum = 0
+        rated_sum_expected = 0
         for device in self.devices:
             data = np.array(device['cost_data'])
-            load_max_real = data[:, 0].max()
-            if device['load_max'] > load_max_real:
-                device['load_max_input'] = device['load_max']
-                device['load_max'] = load_max_real
+            x = data[:, 0]
+            device['load_max_real'] = min(device['load_max'], x.max()) / 100
+            device['load_min_real'] = max(device['load_min'], x.min()) / 100
+            device['data'] = data
+            device['data'][:, 0] /= 100
+            device['load_max'] /= 100
+            device['load_min'] /= 100
+            device['output_max'] = device['load_max_real'] * device['rated_output']
+            rated_sum += device['output_max']
+            rated_sum_expected += device['load_max'] * device['rated_output']
+
+        if rated_sum < self.target_output:
+            msg = 'The maximum rated output of all device is less than \'target_output\'. \n'
+            if rated_sum_expected >= self.target_output:
+                msg += 'Some device\'s max load in \'cost_data\' is less than [\'load_max\']. Please check.'
+            raise ParamError(msg)
+        self._rated_sum = rated_sum
+
+
 
 
 def call(*args, **kwargs):
